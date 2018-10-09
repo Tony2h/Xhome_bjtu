02d1072515cdb79dd3e566b475d39f8a4ae266de
tony zhang <34825804+Tony2h@users.noreply.github.com>
2018-10-09 22:37:11 +0800

    Delete readFromCsvToTxt.py

--- readFromCsvToTxt.py
+++
@@ -1,8 +0,0 @@
-with open("playlist_style1.csv") as file:
-	for line in file:
-		line1=line.split('\n')
-		str = line1[0].split(',')
-		songname = str[0]+'\n'
-		with open("songname.txt",'a') as f: 
-    			f.write(songname)
-		
