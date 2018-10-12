92edc6a58f813579870e3e7fe2a7ee37e9113fdf
Wu Jiahang <40447174+JiahangOK@users.noreply.github.com>
2018-10-12 13:20:00 +0800

    Add files via upload

---
+++ webchat.py
@@ -0,0 +1,9 @@
+import os
+
+from rasa_addons.webchat import WebChatInput, SocketInputChannel
+from rasa_core.agent import Agent
+current_path = os.path.dirname(os.path.realpath(__file__))
+
+agent = Agent.load("models/dialogue", "models/default/current")
+input_channel = WebChatInput(static_assets_path=os.path.join(current_path, 'static'))
+agent.handle_channel(SocketInputChannel(5002, "/", input_channel))
